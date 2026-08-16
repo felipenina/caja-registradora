7fd44597fe46d7ce4d215e87a4a714544916ba64
felipenina <fnina2645@gmail.com>
2026-08-16 10:05:05 -0300

    etapa 2: carga de un producto

--- Program.cs
+++ Program.cs
@@ -10,5 +10,60 @@ string cajero = Console.ReadLine();
 // 3. Saludo con interpolación de cadenas ($)
 Console.WriteLine($"Bienvenida/o, {cajero}. Caja abierta.");
 
+// --- ETAPA 2: Cargar un producto ---
+// 1. Pedir el nombre del producto
+Console.Write("Producto: ");
+string? producto = Console.ReadLine();
+
+// 2. Pedir el precio y convertirlo a decimal
+Console.Write("Precio: ");
+decimal precio = decimal.Parse(Console.ReadLine() ?? "0");
+
+// 3. Mostrar el producto cargado con su precio
+Console.WriteLine($"Agregado: {producto} - ${precio}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Pausa final para que no se cierre la consola al ejecutar
 Console.ReadLine();
